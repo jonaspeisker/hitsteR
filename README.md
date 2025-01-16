bdcfdefde92ebf4914ec9dfcf132c99fb9ef86a1
jonaspeisker <mail@jonaspeisker.de>
2025-01-16 11:12:58 +0100

    wrap code in functions

--- README.md
+++ README.md
@@ -3,23 +3,28 @@
 [![Language: R](https://img.shields.io/badge/Language-R-blue?logo=r)][r]
 [![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)][gplv3]
 
-A simple R script to make a game similar to [Hitster][hitster] and [QRSong!][qrsong]. It takes a Spotify playlist as input and generates a pdf with artist, year, and song title on one side and a QR code to play the song on the other side.
+R functions to make a game similar to [Hitster][hitster] and [QRSong!][qrsong]. Based on a Spotify playlist, it generates a pdf with artist, year, and song title on one side and a QR code to play the song on the other side.
 
 ## Steps
 
 1. Create a Spotify playlist or select a public one that you want to turn into a game. The years of the songs should be approximately uniformly distributed over several decades. 
-1. Assign your [Spotify client ID and client secret][spotify-dev] to `my_client_id` and `my_client_secret` in `.Renviron`. 
-2. Set the [Spotify ID][playlist-id] of the playlist.
-3. Run the script.
-4. Print the pdf saved in output/ double-sided, mirrored on the long edge. For best results use heavy paper or laminate the pages.
+1. Assign your [Spotify client ID and client secret][spotify-dev] to `my_client_id` and `my_client_secret` in .Renviron. 
+3. Run the hitster.R to download and clean the track info with get_tracks() and create a pdf of playing cards with make_cards().
+4. Print the pdf double-sided and mirrored on the long edge. For best results use heavy paper or laminate the pages.
 5. Cut pages along the marks.
 6. Enjoy!
 
-## Options
+## Functions
 
-- `card_size`
-  - `"small"`: 3.8 cm, 12 cards per DIN A4 page (default)
-  - `"original"`: 6.5 cm, 35 cards
+- get_tracks(playlist_id): download track info from Spotify and prepare it for plotting 
+  - playlist_id: a [Spotify playlist ID][playlist-id] (default: [Top 100 Greatest Songs of All Time](https://open.spotify.com/playlist/6i2Qd6OpeRBAzxfscNXeWp?si=b7546d23b6284203)
+
+- make_cards(tracks, file, card_size, paper_size, color): make a pdf of playing cards
+  - tracks: a data frame created with get_tracks()
+  - file: a valid path to a pdf file (default: NULL, save file in output/)
+  - card_size: "small" (3.8 cm) or "original" (6.5 cm) (default: "small")
+  - paper_size = "a4" or "letter" (default: "a4"),
+  - color: color font based on year (default: FALSE), planned
 
 ## License
 
---
+++ hitster_fun.R
@@ -0,0 +1,196 @@
+get_tracks <- function(playlist_id = "6i2Qd6OpeRBAzxfscNXeWp"){
+  require(tidyverse)
+  require(spotifyr)
+  
+  # set credentials for Spotify API
+  Sys.setenv(SPOTIFY_CLIENT_ID = my_client_id)
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = my_client_secret)
+  access_token <- get_spotify_access_token()
+  # scopes <- scopes()
+  # scopes[grepl("user-read", scopes) | grepl("playlist", scopes)]
+  my_auth <- 
+    get_spotify_authorization_code(
+      c("playlist-read-private","playlist-read-collaborative"))
+  
+  # iterate over pages since the API only returns 100 entries at a time
+  tracks_raw <- tibble()
+  for (page in seq(0, 200, 100)) {
+    tmp <- get_playlist_tracks(playlist_id, offset = page)
+    tracks_raw <- bind_rows(tracks_raw, tmp)
+  }
+  
+  # clean data and paste together artist names
+  # only the first two artists are considered
+  tracks <-
+    tracks_raw %>%
+    unnest_wider(track.artists) %>% 
+    unnest_wider(name, names_sep = "artist") %>%
+    mutate(
+      artist = if_else(
+        is.na(nameartist2), 
+        nameartist1, 
+        paste(nameartist1, nameartist2, sep = " & ")
+      ),
+      year = as.integer(substr(track.album.release_date, 1, 4)),
+      track = track.name,
+      url = track.external_urls.spotify,
+      .keep = "none"
+    ) 
+  print(paste0("Got ", nrow(tracks), " tracks."))
+  print("Distribution of years:")
+  print(quantile(tracks$year))
+  return(tracks)
+}
+
+make_cards <- function(
+    tracks,
+    file = NULL,
+    card_size = "small",
+    paper_size = "a4",
+    color = FALSE
+) {
+  require(grid)
+  require(gridtext)
+  require(ggqr)
+  require(Cairo)
+  
+  if (paper_size == "a4") {
+    paper_width <- 21
+    paper_height <- 29.7
+  } else if (paper_size == "letter"){
+    paper_width <- 21.6
+    paper_height <- 27.9
+  } else {
+    stop("Paper size should be 'a4' or 'letter'.")
+  }
+  
+  if (card_size == "small") {
+    card_width <- 3.8
+    small_font_size <- 9
+    large_font_size <- 32
+  } else if (card_size == "original") {
+    card_width <- 6.5
+    small_font_size <- 12
+    large_font_size <- 60
+  } else {
+    stop("Card size should be 'small' or 'original'.")
+  }
+  
+  if (is.null(file)) {
+    out <- paste0("output/hitster_", card_size, "_", paper_size, ".pdf")
+  } else if (is.character(file)) {
+    out <- file
+  } else {
+    stop("File should be NULL or a string.")
+  }
+  
+  # number of cards along the axes
+  n_cards_x <- paper_width %/% card_width
+  n_cards_y <- paper_height %/% card_width
+  cards_per_page <- n_cards_x * n_cards_y
+  pages <- ceiling(nrow(tracks) / cards_per_page)
+  # margins on each side of the axes
+  margin_x <- (paper_width %% card_width) / 2
+  margin_y <- (paper_height %% card_width) / 2
+  
+  # left hand page
+  # grid coords
+  gx_left <- unit(seq(margin_x, paper_width, card_width), "cm")
+  gy_left <- unit(seq(margin_y, paper_height, card_width), "cm")
+  # content coords
+  x_left <- rep(gx_left[-1] - unit(card_width/2, "cm"), times = length(gy_left)-1)
+  y_left <- rep(gy_left[-1] - unit(card_width/2, "cm"), each = length(gx_left)-1)
+  # data.frame(x_left,y_left)
+  
+  # right hand page (mirrored on y axis)
+  gx_right <- unit(seq(paper_width - margin_x, 0, -card_width), "cm")
+  gy_right <- gy_left
+  x_right <- rep(gx_right[-1] + unit(card_width/2, "cm"), times = length(gy_right)-1)
+  y_right <- rep(gy_right[-1] - unit(card_width/2, "cm"), each = length(gx_right)-1)
+  # data.frame(x_right,y_right)
+  
+  #### make pdf ####
+  Cairo(
+    width = paper_width, 
+    height = paper_height, 
+    file = out, 
+    type = "pdf", 
+    units = "cm"
+  )
+  for (page in 1:pages) {
+    # make left hand page with track info
+    grid.newpage()
+    # make lines for cutting
+    grid.grill(v = gx_left, h = gy_left, gp=gpar(col="grey"))
+    # iterate over cards on each page
+    for (i in 1:cards_per_page) {
+      track_num <- i + cards_per_page * (page - 1)
+      
+      # create viewport for artist
+      artist_vp <- viewport(
+        x = x_left[i], y = y_left[i] + unit(card_width, "cm") / 3,
+        width = unit(card_width, "cm"), height = unit(card_width, "cm") / 3, 
+        clip = "on" # avoid text on the neighboring cards
+      )
+      artist_grob <- textbox_grob(
+        vp = artist_vp,
+        text = tracks$artist[track_num],
+        width = unit(card_width, "cm"), height = unit(card_width, "cm") / 3, 
+        hjust = 1, vjust = 1, halign = 0.5, valign = 0.5,
+        margin = unit(rep(2,4), "pt"),
+        gp = gpar(lineheight=0.9, fontsize=small_font_size)#,
+        # box_gp = gpar(col = "black", fill = "lightblue")
+      )
+      grid.draw(artist_grob)
+      
+      # create viewport for year        
+      year_vp <- viewport(
+        x = x_left[i], y = y_left[i],
+        width = unit(card_width, "cm"), height = unit(card_width, "cm") / 3, 
+        clip = "on" # avoid text on the neighboring cards
+      )
+      year_grob <- textbox_grob(
+        vp = year_vp,
+        text = tracks$year[track_num],
+        width = unit(card_width, "cm"), height = unit(card_width, "cm") / 3, 
+        hjust = 1, vjust = 1, halign = 0.5, valign = 0.5,
+        margin = unit(rep(2,4), "pt"),
+        gp = gpar(lineheight=0.9, fontsize=large_font_size)#,
+        # box_gp = gpar(col = "black", fill = "cornsilk")
+      )
+      grid.draw(year_grob)
+      
+      # create viewport for track title
+      title_vp <- viewport(
+        x = x_left[i], y = y_left[i] - unit(card_width, "cm") / 3,
+        width = unit(card_width, "cm"), height = unit(card_width, "cm") / 3, 
+        clip = "on" # avoid text on the neighboring cards
+      )
+      title_grob <- textbox_grob(
+        vp = title_vp,
+        text = tracks$track[track_num],
+        width = unit(card_width, "cm"), height = unit(card_width, "cm") / 3, 
+        hjust = 1, vjust = 1, halign = 0.5, valign = 0.5,
+        margin = unit(rep(2,4), "pt"),
+        gp = gpar(lineheight=0.9, fontsize=small_font_size)#,
+        # box_gp = gpar(col = "black", fill = "lightgreen")
+      )
+      grid.draw(title_grob)
+    }
+    
+    # make right hand page with qr code
+    grid.newpage()
+    grid.grill(v = gx_right, h = gy_right, gp=gpar(col="grey"))
+    for (i in 1:cards_per_page) {
+      track_num <- i + cards_per_page * (page - 1)
+      qr <- qrGrob(
+        label = tracks$url[track_num], 
+        x = x_right[i], y = y_right[i], 
+        hjust = 0.5, vjust = 0.5, 
+        size = unit(card_width*0.8, "cm")
+      )
+      grid.draw(qr)
+    }
+  }
+  dev.off()
+}
