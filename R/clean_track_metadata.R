c2c48297bb5112552971d6bb615737ba221afaa0
Jonas Peisker <peisker@iiasa.ac.at>
2025-11-21 17:38:04 +0100

    update clean_track_metadata()

--- R/clean_track_metadata.R
+++ R/clean_track_metadata.R
@@ -10,21 +10,23 @@
 clean_track_metadata <- function(tracks_df) {
   tracks <-
     tracks_df |>
-    tidyr::unnest_wider(track.artists) |> 
-    tidyr::unnest_wider(name, names_sep = "artist") |> 
-    dplyr::mutate(
-      artist1 = nameartist1,
-      # only the first two artists are considered due to space limitations
-      artist = ifelse(
-        is.na(nameartist2), 
-        nameartist1, 
-        paste(nameartist1, nameartist2, sep = " & ")
-      ),
-      year = as.integer(substr(track.album.release_date, 1, 4)),
-      track_name = track.name,
-      url = track.external_urls.spotify,
-      .keep = "none"
-    ) |> 
+    tidyr::unnest_longer(track.artists) |> 
+    tidyr::unnest_wider(track.artists) |>
+    dplyr::rename(
+      artist_name = name,
+      url = track.external_urls.spotify
+      ) |>
+    dplyr::group_by(url) |>
+    dplyr::summarise(
+      artist = paste(artist_name, collapse = " & "),
+      year = 
+        track.album.release_date |> 
+        dplyr::first() |> 
+        substr(1, 4) |> 
+        as.integer(),
+      track_name = dplyr::first(track.name),
+      .groups = "drop"
+    ) |>
     dplyr::arrange(year)
   message("Cleaned data of ", nrow(tracks), " tracks.")
   return(tracks)
