63fca20bd2a9d7311c5ca76779ec22af397dea44
Jonas Peisker <peisker@iiasa.ac.at>
2025-11-29 17:49:56 +0100

    import stringr

--- dev/R/helpers.R
+++ dev/R/helpers.R
@@ -3,10 +3,11 @@ library(devtools)
 
 load_all()
 document()
+check()
+
 install()
 build_vignettes()
 build_readme()
-check()
 # .httr-oauth
 # tests/testthat/.httr-oauth
 # "output/history_hitster_small_a4_color.pdf"
